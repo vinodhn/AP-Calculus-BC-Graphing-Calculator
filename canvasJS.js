ce8aaea9f9341386bb43c8d9fd1d5c11c4b8c8a7
Vinodh N <vinodhnagarajan@outlook.com>
2018-09-01 15:48:53 -0700

    Min/Max added and works
    Could highlight min/max a little better but for now its fine

--- canvasJS.js
+++ canvasJS.js
@@ -1,44 +1,11 @@
-var expression, exprCompile, xValues = [], yValues = []
+var expression, exprCompile, xValues = [], yValues = [], xValuesD = [], yValuesD = [], yValuesD2 = []
 var Canvas = document.getElementById('xy-graph')
 var Ctx = null
 var Width = Canvas.width
 var Height = Canvas.height
 var checkbox1 = document.querySelector("input[name=fprime]")
 var checkbox2 = document.querySelector("input[name=f2prime]")
-var checkboxStatus = 0
-
-//These two blocks of code wait until a change occurs in either checkbox and acts accordingly
-checkbox1.addEventListener( 'change', function(){
-  if(this.checked){
-    if(checkbox2.checked){
-      checkboxStatus = 3
-    }else {
-      checkboxStatus = 1
-    }
-  }else{
-    if(checkbox2.checked){
-      checkboxStatus = 2
-    }else {
-      checkboxStatus = 0
-    }
-  }
-})
-
-checkbox2.addEventListener( 'change', function(){
-  if(this.checked){
-    if(checkbox1.checked){
-      checkboxStatus = 3
-    }else {
-      checkboxStatus = 2
-    }
-  }else{
-    if(checkbox1.checked){
-      checkboxStatus = 1
-    }else {
-      checkboxStatus = 0
-    }
-  }
-})
+var checkbox3 = document.querySelector("input[name=minmax]")
 
 // Defines domain and range of the graph
 function MaxX() {
@@ -75,7 +42,11 @@ function Draw() {
  expr = math.compile(expression)
 
  xValues = math.range(-10, 10, XSTEP).toArray()
+ xValuesD = math.range(-10-XSTEP, 10+XSTEP, XSTEP).toArray()
  yValues = xValues.map(function (x) {
+   return expr.eval({x: x})
+ })
+ yValuesD = xValuesD.map(function (x) {
    return expr.eval({x: x})
  })
 
@@ -83,26 +54,18 @@ function Draw() {
    Ctx = Canvas.getContext('2d')
    Ctx.clearRect(0,0,Width,Height)
    DrawAxes()
-   switch(checkboxStatus){
-     case 0:
-      Graph()
-       break;
-     case 1:
-
-       break;
-     case 2:
-
-       break;
-     case 3:
-
-       break;
-     // If everything breaks and just doesn't work, it will just spit out a simple alert
-     default:
-       alert("Sorry, something went wrong")
+   Graph();
+   if(checkbox1.checked){
+     GraphPrime();
    }
-  } else {
-    // Do nothing.
-  }
+   if(checkbox2.checked){
+     GraphPrime2();
+   }
+
+   if(checkbox3.checked){
+     GraphMinMax();
+   }
+}
 }
 //Deltas are distance between tick marks -- used only for drawing the axes
 function XTickDelta() {
@@ -212,6 +175,7 @@ function Graph() {
   var first = true
 
   Ctx.beginPath()
+  Ctx.fillStyle = '#000000'
   for (var i = 0;i < xValues.length;i++) {
    var x = xValues[i]
    var y = yValues[i]
@@ -221,6 +185,85 @@ function Graph() {
    } else {
     Ctx.fillRect(XC(x),YC(y),1,1)
    }
+  }
+  Ctx.stroke()
+}
+
+//Calculate and Graph Derivative
+function GraphPrime() {
+  var first = true
+
+  Ctx.beginPath()
+  Ctx.fillStyle = '#FF0000'
+  for(var i = 0; i < xValuesD.length;i++){
+    var x = xValuesD[i+1]
+    var y2 = yValuesD[i], y1 = yValuesD[i+2]
+    var x2 = xValuesD[i], x1 = xValuesD[i+2]
+    var y = (y2-y1)/(x2-x1)
+    if(first) {
+      Ctx.moveTo(XC(x),YC(y))
+      first = false
+    } else {
+      Ctx.fillRect(XC(x), YC(y), 1, 1)
+    }
+  }
+  Ctx.stroke()
+}
+
+//Calculate and Graph Derivative 2
+function GraphPrime2() {
+  var first = true
+
+  for(var i = 0; i < xValuesD.length;i++){
+    var x = xValuesD[i+1]
+    var y2 = yValuesD[i], y1 = yValuesD[i+2]
+    var x2 = xValuesD[i], x1 = xValuesD[i+2]
+    var y = (y2-y1)/(x2-x1)
+    if(i < xValues.length){
+      yValuesD2[i] = y
+    }
+  }
+
+  Ctx.beginPath()
+  Ctx.fillStyle = '#0000FF'
+  for(var i = 0; i < xValuesD.length;i++){
+    var x = xValuesD[i+1]
+    var y2 = yValuesD2[i], y1 = yValuesD2[i+2]
+    var x2 = xValuesD[i], x1 = xValuesD[i+2]
+    var y = (y2-y1)/(x2-x1)
+    if(first) {
+      Ctx.moveTo(XC(x),YC(y))
+      first = false
+    } else {
+      Ctx.fillRect(XC(x), YC(y), 1, 1)
+    }
+  }
+  Ctx.stroke()
+}
+
+function GraphMinMax() {
+
+  for(var i = 0; i < xValuesD.length;i++){
+    var x = xValuesD[i+1]
+    var y2 = yValuesD[i], y1 = yValuesD[i+2]
+    var x2 = xValuesD[i], x1 = xValuesD[i+2]
+    var y = (y2-y1)/(x2-x1)
+    if(i < xValues.length){
+      yValuesD2[i] = Math.round(y*1000)/1000;
+      console.log(x + "  " + yValuesD2[i])
+    }
+  }
+
+  Ctx.beginPath()
+  Ctx.fillStyle = '#00FF00'
+  for(var i = 0; i < xValuesD.length; i++){
+    if(Math.round(yValuesD2[i]* 1000)/1000 == 0.000){
+      console.log("HELLO")
+      var x = Math.round(xValuesD[i+1] * 10000)/ 10000
+      var y = Math.round(yValues[i+1] * 10000) / 10000
+      Ctx.moveTo(XC(x),YC(y))
+      Ctx.fillRect(XC(x), YC(y), 1, 3)
+    }
   }
   Ctx.stroke()
 }
